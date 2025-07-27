e9832ad776022e3c8e79db8abe1114c1ec540958
rv-sl <raveemox@gmail.com>
2025-07-27 19:26:05 +0530

    Create callback_data.py

---
+++ plugins/callback_data.py
@@ -0,0 +1,12 @@
+import hashlib
+
+# You can replace this with a persistent database or file
+callback_data_store = {}
+
+def store_callback_data(data: str) -> str:
+    key = hashlib.md5(data.encode()).hexdigest()
+    callback_data_store[key] = data
+    return key
+
+def get_callback_data(key: str) -> str:
+    return callback_data_store.get(key)
