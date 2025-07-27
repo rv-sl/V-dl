259e856a61db3e1543f46cf3368cbc20c1abeafd
rv-sl <raveemox@gmail.com>
2025-07-27 16:54:45 +0530

    Create auther.py

---
+++ plugins/auther.py
@@ -0,0 +1,9 @@
+import os
+from pyrogram import filters
+
+# Load authorized user IDs from environment
+raw_ids = os.getenv("AUTHORIZED_USERS", "")
+AUTHORIZED_USERS = [int(uid.strip()) for uid in raw_ids.split(",") if uid.strip().isdigit()]
+
+def is_authorized():
+    return filters.user(AUTHORIZED_USERS)
