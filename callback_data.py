fa7e2fa73bd89183069698e51b7db55731b96e58
rv-sl <raveemox@gmail.com>
2025-07-27 19:49:06 +0530

    Update callback_data.py

--- callback_data.py
+++ callback_data.py
@@ -4,7 +4,7 @@ import hashlib
 callback_data_store = {}
 
 def store_callback_data(data: str) -> str:
-    key = hashlib.md5(data.encode()).hexdigest()
+    key = hashlib.md5(data.encode()).hexdigest()[:10]
     callback_data_store[key] = data
     return key
 
