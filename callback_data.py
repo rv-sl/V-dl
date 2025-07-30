8e541cae7b75fd146784d0fc039a47a225b5236d
rv-sl <raveemox@gmail.com>
2025-07-30 07:00:58 +0530

    Update callback_data.py

--- callback_data.py
+++ callback_data.py
@@ -3,8 +3,11 @@ import hashlib
 # You can replace this with a persistent database or file
 callback_data_store = {}
 
-def store_callback_data(data: str) -> str:
-    key = hashlib.md5(data.encode()).hexdigest()[:10]
+def store_callback_data(data: dict) -> str:
+    url = data.get("url")
+    if not url:
+        return None
+    key = hashlib.md5(url.encode()).hexdigest()[:10]
     callback_data_store[key] = data
     return key
 
